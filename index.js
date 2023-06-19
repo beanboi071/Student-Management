c197724c0755a1fc6faa730bab2edac3f07fd6d6
rojanshakya071 <rojan@riddhasoft.com>
2023-06-19 21:33:20 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,81 @@
+function Student(name, age, gender, faculty) {
+    var self = this;
+  
+    self.name = ko.observable(name);
+    self.age = ko.observable(age);
+    self.gender = ko.observable(gender);
+    self.faculty = ko.observable(faculty);
+  }
+  
+  function AppViewModel() {
+    var self = this;
+  
+    self.faculties = ["Science", "Arts", "Commerce"];
+  
+    self.students = ko.observableArray([
+      new Student("Rojan Shakya", 20, "Male", "Science"),
+      new Student("Sita", 22, "Female", "Arts"),
+      new Student("Rajesh", 19, "Male", "Commerce")
+    ]);
+  
+    self.newStudent = {
+      name: ko.observable(''),
+      age: ko.observable(''),
+      gender: ko.observable(''),
+      faculty: ko.observable('')
+    };
+  
+    self.addStudent = function() {
+      var name = self.newStudent.name();
+      var age = self.newStudent.age();
+      var gender = self.newStudent.gender();
+      var faculty = self.newStudent.faculty();
+   
+        self.students.push(new Student(name, age, gender, faculty));
+        self.newStudent.name('');
+        self.newStudent.age('');
+        self.newStudent.gender('');
+        self.newStudent.faculty(''); 
+    };
+  
+    self.editStudent = function(student) {
+        self.editedStudent.name(student.name());
+        self.editedStudent.age(student.age());
+        self.editedStudent.gender(student.gender());
+        self.editedStudent.faculty(student.faculty()); 
+
+        document.getElementById("editForm").style.display = "block";
+      };
+    
+      self.applyChanges = function() {
+        var editedName = self.editedStudent.name();
+        var editedAge = self.editedStudent.age();
+        var editedGender = self.editedStudent.gender();
+        var editedFaculty = self.editedStudent.faculty();
+     
+        var student = ko.utils.arrayFirst(self.students(), function(item) {
+          return item.name() === self.editedStudent.name();
+        });
+    
+        if (student) {
+          student.name(editedName);
+          student.age(editedAge);
+          student.gender(editedGender);
+          student.faculty(editedFaculty);
+        }
+     
+        self.editedStudent.name('');
+        self.editedStudent.age('');
+        self.editedStudent.gender('');
+        self.editedStudent.faculty('');
+     
+        var editForm = document.getElementById('editForm');
+        editForm.style.display = 'none';
+      };
+  
+    self.deleteStudent = function(student) { 
+        self.students.remove(student); 
+    };
+  }
+  
+  ko.applyBindings(new AppViewModel());
